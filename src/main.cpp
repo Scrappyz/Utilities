24a3a71841b76d64a54853b7b94b49a53808bd12
Scrappyz <ghostmic3000@gmail.com>
2023-06-28 21:41:06 +0800

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    
+    CLI cli;
+    try {
+        cli.setValidSubcommands({"stuff", "ei"});
+        cli.setArguments({"MyProgram", "-hmv", "--help", "stuff"});
+        cli.setValidSubcommands({});
+        cli.setValidFlags({"-h", "--help", "-m", "-v"});
+        cli.printSubcommandsAndFlags();
+    } catch(const CLIException& e) {
+        cout << e.what() << endl;
+    }
     
     return 0;
 }
