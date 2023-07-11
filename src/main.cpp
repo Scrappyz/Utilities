442b07194f8f43d44785811eaf9a370d87241113
Scrappyz <ghostmic3000@gmail.com>
2023-07-11 21:34:04 +0800

    Added config.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include "path.hpp"
+#include "config.hpp"
 
 using namespace std;
 
+void printConfig(const std::unordered_map<std::string, std::unordered_map<std::string, std::string>>& keyval)
+{
+    for(const auto& i : keyval) {
+        std::cout << "[" << i.first << "]" << std::endl;
+        for(const auto& j : i.second) {
+            std::cout << "    " << j.first << " = " << j.second << std::endl;
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    path::open("CMakeLists.txt", "C:\\Users\\Michael Angelo\\AppData\\Local\\Programs\\Microsoft VS Code\\bin\\code");
+    std::string config_path = path::join(path::sourcePath(), "config.txt");
+    Config config(config_path);
+    std::cout << config.getValueOf("wey") << std::endl;
+    std::cout << config.getValueOf("mykey") << std::endl;
+    std::cout << config.getValueOf("new section", "new key") << std::endl;
     
     return 0;
 }
