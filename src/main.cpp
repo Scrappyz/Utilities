7ed278fd9dc5bbd657183fc3ea3fb82879fc5c8b
Scrappyz <ghostmic3000@gmail.com>
2023-07-13 21:22:00 +0800

    Removed unneeded headers

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,10 @@ using namespace std;
 
 int main()
 {
-    std::string file_path = path::join(path::sourcePath(), "temp/test.txt");
-    Config::modifyValueInFile(file_path, "section 1", "s2", "D:/Docume h Code");
-    Config::modifyValueInFile(file_path, "section 1", "s3", "cunn");
-    Config::modifyValueInFile(file_path, "nokey", "cookie");
-    std::cout << Config::modifyValueInFile(file_path, "s2", "new val") << std::endl;
+    std::string config_path = path::join(path::sourcePath(), "config.txt");
+    Config config(config_path);
+    std::cout << config.getValueOf("key1") << std::endl;
+    std::cout << config.getValueOf("Section 1", "key2") << std::endl;
+    std::cout << config.getValueOf("Section 2", "key 3") << std::endl;
+    std::cout << config.getValueOf("Section 2", "key4") << std::endl;
 }
