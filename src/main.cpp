5fc0d12f723c2c2b5483702ee8ec88b69df256c1
Scrappyz <ghostmic3000@gmail.com>
2023-07-30 13:41:24 +0800

    Copy now creates a directory if not exists

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 
 int main()
 {
-    std::string from = path::joinPath(path::sourcePath(), "./");
-    std::string to = path::joinPath(path::sourcePath(), "a/b/c");
+    std::string from = path::joinPath(path::sourcePath(), "temp");
+    std::string to = path::joinPath(path::sourcePath(), "temp/a/b/c");
+    // std::filesystem::copy(from, to, std::filesystem::copy_options::recursive);
     path::copy(from, to);
     
     return 0;
